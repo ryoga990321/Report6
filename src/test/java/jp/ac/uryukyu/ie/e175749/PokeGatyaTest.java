6aa098e1d4641c3266402631429454d0dab19208
Ryoga Gushiken <e175749@ie.u-ryukyu.ac.jp>
2018-01-14 02:22:04 +0900

    fifth commit

---
+++ src/test/java/jp/ac/uryukyu/ie/e175749/PokeGatyaTest.java
@@ -0,0 +1,13 @@
+package jp.ac.uryukyu.ie.e175749;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PokeGatyaTest {
+    @Test
+    public void celect() {
+
+    }
+
+}
